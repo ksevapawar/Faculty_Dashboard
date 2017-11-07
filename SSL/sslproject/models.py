b02a05a1339e1d950fba7befea04c1d73729257f
harshit4567 <harshitgupta@iitg.ernet.in>
2017-11-07 22:26:30 +0530

    syncing signup with database

--- SSL/sslproject/models.py
+++ SSL/sslproject/models.py
@@ -1,9 +1,22 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
 
 
 class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     department = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name_plural = "User Profiles"
 
+    def __unicode__(self):
+        return self.user.username
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Employee.objects.create(user=instance)
+
+
+post_save.connect(create_user_profile, sender=User)
--- SSL/sslproject/views.py
+++ SSL/sslproject/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
-
+from sslproject.models import Employee
+from sslproject.forms import SignUpForm
 # Create your views here.
 
 from django.contrib.auth import login, authenticate
@@ -14,17 +16,22 @@ def index(request):
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
+            u= User.objects.get(username=username)
+            m=Employee.objects.get(user__username=username)
+            m.department=form.cleaned_data.get('department')
+            m.save()
             user = authenticate(username=username, password=raw_password)
             login(request, user)
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
+
 
 def logout_view(request):
     logout(request)
